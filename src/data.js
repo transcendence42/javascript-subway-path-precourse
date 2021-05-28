25065c8fa74ea330b4474e98fc92be75d75d1f4d
hochan Lee <hochan049@gmail.com>
2021-05-28 16:53:25 +0900

    feat: 기능 구현 초기설정으로 data.js 파일 생성 요구사항에 맞는 line 과 station data 생성 완료

--- src/data.js
+++ src/data.js
@@ -1,26 +1,86 @@
-export const users = [
+export const station = [
   {
-    name: 'Alt',
+    name: '교대',
   },
   {
-    name: 'Jamie',
+    name: '강남',
   },
   {
-    name: 'Sony',
+    name: '역삼',
+  },
+  {
+    name: '남부터미널',
+  },
+  {
+    name: '양재',
+  },
+  {
+    name: '양재시민의숲',
+  },
+  {
+    name: '매봉',
   },
 ];
 
-export const courses = [
+export const line = [
   {
-    name: 'frontend',
+    name: '2호선',
+    station: ['교대', '강남', '역삼'],
+    cost: [
+      {
+        'ascending-pathway': '교대',
+        'descending-pathway': '강남',
+        'path-cost': 2,
+        'time-cost': 3,
+      },
+      {
+        'ascending-pathway': '강남',
+        'descending-pathway': '역삼',
+        'path-cost': 2,
+        'time-cost': 3,
+      },
+    ],
   },
   {
-    name: 'backend',
+    name: '3호선',
+    station: ['교대', '남부터미널', '양재', '매봉'],
+    cost: [
+      {
+        'ascending-pathway': '교대',
+        'descending-pathway': '남부터미넣',
+        'path-cost': 3,
+        'time-cost': 2,
+      },
+      {
+        'ascending-pathway': '남부터미널',
+        'descending-pathway': '양재',
+        'path-cost': 6,
+        'time-cost': 5,
+      },
+      {
+        'ascending-pathway': '양재',
+        'descending-pathway': '매봉',
+        'path-cost': 1,
+        'time-cost': 1,
+      },
+    ]
   },
   {
-    name: 'iOS',
-  },
-  {
-    name: 'Android',
+    name: '신분당선',
+    station: ['강남', '양재', '양재시민의숲'],
+    cost: [
+      {
+        'ascending-pathway': '강남',
+        'descending-pathway': '양재',
+        'path-cost': 2,
+        'time-cost': 8,
+      },
+      {
+        'ascending-pathway': '양재',
+        'descending-pathway': '양재시민의숲',
+        'path-cost': 10,
+        'time-cost': 3,
+      },
+    ]
   },
 ];
